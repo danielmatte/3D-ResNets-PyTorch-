3541c494670294543f3837e0647a656c1c5a93ba
Daniel <daniel.freitas@ic.unicamp.br>
2019-07-09 16:48:24 -0400

    Evaluate UCF101

---
+++ utils/video_eval_ucf101.py
@@ -0,0 +1,16 @@
+import sys
+from eval_ucf101 import UCFclassification
+
+if __name__ == '__main__':
+  if len(sys.argv) < 3:
+    print("Usage: python3 {} annotation_filename prediction_filename".format(sys.argv[0]))
+
+
+  ucfClassification = UCFclassification(ground_truth_filename=sys.argv[1], 
+                                        prediction_filename=sys.argv[2], 
+                                        subset='validation', 
+                                        verbose=True, 
+                                        top_k=5)
+
+  ucfClassification.evaluate()
+  print(ucfClassification.hit_at_k)
