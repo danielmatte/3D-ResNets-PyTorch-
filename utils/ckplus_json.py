94272a1420c5f1f9f4e8a00bf7ffa3d0069101b9
Daniel <daniel.freitas@ic.unicamp.br>
2019-07-11 11:27:37 -0400

    ck+ to  activitynet json

--- utils/ckplus_json.py
+++ utils/ckplus_json.py
@@ -3,37 +3,54 @@ import os
 from glob import glob
 import json
 
-def getEmotionFromFile(filename):
+def getEmotionFromFile(dataset_path, filename):
   emotions = {0: 'neutral', 1: 'anger', 2: 'contempt', 3: 'disgust', 4: 'fear', 5: 'happy', 6: 'sadness', 7: 'surprise'}
-  with open(filename) as f:
-    emotion_id = int(float(f.readline()))
-    return emotions[emotion_id], emotion_id
 
-def getVideoPath(dataset_path, emotionFilename):
-    subject = emotionFilename.split('/')[-3]
-    subject_video = emotionFilename.split('/')[-2]
-    video_path = "cohn-kanade-images/{}/{}".format(subject, subject_video)
+  subject = filename.split('/')[-3]
+  session = filename.split('/')[-2]
+  emotionPath = "{}/Emotion/{}/{}/*.txt".format(dataset_path, subject, session)
+  for filename in glob(pathname=emotionPath, recursive=True):
+    with open(filename) as f:
+      emotion_id = int(float(f.readline()))
+      return emotions[emotion_id], emotion_id
+  return None, None
+
+def getVideoPath(dataset_path, filename):
+    subject = filename.split('/')[-3]
+    session = filename.split('/')[-2]
+    video_path = "cohn-kanade-images/{}/{}".format(subject, session)
     if (os.path.isdir(dataset_path+video_path)):
       return video_path
     else:
       return None
 
+def getAUs(filename):
+  AUs = []
+  with open(filename) as f:
+    for line in f:
+      if len(line) > 5:
+        AUs.append(int(float(line.split()[0])))
+  return AUs
+  
 def getDataset(dataset_path):
-  emotionsPath = "{}/Emotion/**/*.txt".format(dataset_path)
+  FACSPath = "{}/FACS/**/*.txt".format(dataset_path)
   labels = []
   dataset = {}
   dataset['labels'] = []
   dataset['database'] = {}
-  for filename in glob(pathname=emotionsPath, recursive=True):
+  for filename in glob(pathname=FACSPath, recursive=True):
     video_path = getVideoPath(dataset_path, filename)
     if (video_path):
-      label, _ = getEmotionFromFile(filename)
-      if label not in labels:
-        labels.append(label)
-
       dataset['database'][video_path] = {}
       dataset['database'][video_path]['subset'] = 'training'
-      dataset['database'][video_path]['annotations'] = {'label': label}
+      dataset['database'][video_path]['annotations'] = {}
+      label, _ = getEmotionFromFile(dataset_path, filename)
+      if label:
+        dataset['database'][video_path]['annotations']['label'] = label
+        if label not in labels:
+          labels.append(label)
+
+      dataset['database'][video_path]['annotations']['AUs'] = getAUs(filename)
 
   dataset['labels'] = labels
   return dataset
