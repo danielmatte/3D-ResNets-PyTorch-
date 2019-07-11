d395f7b7f6d84d1864a51f61f3d8ee2839d650e6
Daniel <daniel.freitas@ic.unicamp.br>
2019-07-11 15:00:00 -0400

    bug fix

--- utils/video_eval_ucf101.py
+++ utils/video_eval_ucf101.py
@@ -4,7 +4,7 @@ from eval_ucf101 import UCFclassification
 if __name__ == '__main__':
   if len(sys.argv) < 3:
     print("Usage: python3 {} annotation_filename prediction_filename".format(sys.argv[0]))
-
+    exit(0)
 
   ucfClassification = UCFclassification(ground_truth_filename=sys.argv[1], 
                                         prediction_filename=sys.argv[2], 
